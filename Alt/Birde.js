ab6cfd180a0a94035142d04a13a489d32fbda470
Mitchell Thompson <leonsbuddydave@gmail.com>
2012-11-19 12:39:01 -0600

    Selectors, ActorGroup, Events updated.
    Extended multiple class selection, added binding for events and
    subevents.

--- Alt/Birde.js
+++ Alt/Birde.js
@@ -1,9 +1,21 @@
+window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+
 (function(w)
 {
 	
 	// Birde.Core
 	var Birde = function(selector)
 	{
+		selector = selector || "*";
 		return new Birde.fn.select(selector);
 	}
 
@@ -12,26 +24,66 @@
 		return Birde.fn.init(props);
 	}
 
+	Birde.Start = function()
+	{
+		if (Initialized)
+			return new Birde.fn.step(0);
+		else
+			return null;
+	}
+
+	var Tick = 15;
+	var Initialized = false;
+	var lastFrameTime = 0;
+
+	var Options =
+	{
+		Capped : true,
+		ClearColor : "#ccc",
+		Canvas : null,
+		Context : null
+	}
+
+	var Modules =
+	{
+		Drawing : null,
+		Input : null
+	}
+
 	Birde.fn = Birde.prototype =
 	{
-		options : {},
 		init : function(props)
 		{
-			Birde.fn.options = Birde.fn.Props.ExtendDefaults(props,
-			{
-				canvas : null
-			});
+			Options = Birde.fn.Props.ExtendDefaults(props, Options);
+
+			Modules.Drawing = new Drawing(Options);
+			Modules.Input = new Input(Options);
+
+			Initialized = true;
 
 			return this;
 		},
 
 		select : function(selector, props)
 		{
-			if (selector === "*")
-				return Birde.fn.Scene.Actors;
+			if (selector == "*")
+			{
+				// Converts the scenegraph into an ActorGroup result
+				var result = new ActorGroup();
+
+				var A = Birde.fn.Scene.Actors;
+
+				for (var key in A)
+				{
+					result.push(A[key]);
+				}
+
+				return result;
+			}
 			else if (selector[0] == ".")
 			{
-				var c = selector.replace(".", "");
+				// Searches for all elements containing the provided class
+				var c = selector.replace(/\./g, "").split(' ');
 
 				// Class selection
 				var results = new ActorGroup();
@@ -40,8 +92,18 @@
 
 				for (var id in A)
 				{
-					if (A[id].class == c)
+					var matchedClasses = 0;
+
+					for (var classIndex in A[id].class)
+					{
+						if (c.indexOf( A[id].class[classIndex] ) != -1)
+							matchedClasses++;
+					}
+
+					if (matchedClasses == c.length)
+					{
 						results.push(A[id]);
+					}
 				}
 
 				return results;
@@ -52,8 +114,7 @@
 
 				// ID Selection
 				if (typeof Birde.fn.Scene.Actors[ id ] != 'undefined' )
-				{
-					console.log("Match found.");
+				{	
 					var a = new ActorGroup();
 					a.push( Birde.fn.Scene.Actors[ id ] );
 					return a;
@@ -67,15 +128,112 @@
 			}
 		},
 
-		step : function(lastTime)
+		step : function()
 		{
-			
-		}
+			var newFrameTime = (new Date()).getTime();
+
+			if (lastFrameTime == 0)
+			{
+				Tick = 0;
+			}
+			else
+			{
+				Tick = ( newFrameTime - lastFrameTime ) / 1000;
+			}
+
+			lastFrameTime = newFrameTime;
+
+			for (key in EventRegistry.step)
+			{
+				var a = EventRegistry.step[key];
+				a.response.call(a.target, Tick);
+			}
+
+			Birde.fn.draw();
+
+			if (Options.Capped)
+				requestAnimFrame(Birde.fn.step)
+			else
+				Birde.fn.step();
+		},
+
+		draw : function()
+		{
+
+			Modules.Drawing.Clear();
+
+			for (key in EventRegistry.draw)
+			{
+				var a = EventRegistry.draw[key];
+				a.response.call(a.target, Modules.Drawing);
+			}
+		},
 	}
 
 	Birde.fn.Scene =
 	{
 		Actors : []
+	}
+
+	// Drawing object, high-level interface for canvas drawing
+	var Drawing = function(props)
+	{
+		props.Canvas = document.getElementById( props.Canvas );
+		this.Context = props.Context = props.Canvas.getContext('2d');
+		this.ClearColor = props.ClearColor;
+		this.Width = props.Canvas.width;
+		this.Height = props.Canvas.height;
+
+		this.Clear = function()
+		{
+			this.Context.fillStyle = this.ClearColor;
+			this.Context.fillRect(0, 0, this.Width, this.Height);
+			this.Context.fill();
+		}
+
+		this.DrawBounds = function(actor)
+		{
+			this.Context.fillStyle = "#f00";
+			this.Context.fillRect(actor.x, actor.y, actor.w, actor.h);
+			this.Context.fill();		
+		}
+	}
+
+	// Input object, monitors and stores everything related to input
+	var Input = function(props)
+	{
+		this.Keystates = [];
+
+		var i = 0;
+		while (i < 128)
+		{
+			this.Keystates[i] = false;
+			i++;
+		}
+
+		document.body.onkeydown = function(e)
+		{
+			// Sets key-down state
+			Modules.Input.Keystates[e.keyCode] = true;
+
+			for (var key in EventRegistry.keydown)
+			{
+				var a = EventRegistry.keydown[key];
+				a.response.call(a.target, e);
+			}
+
+			for (var key in SubEventRegistry.keydown[e.keyCode])
+			{
+				var a = SubEventRegistry.keydown[e.keyCode][key];
+				a.response.call(a.target, e);
+			}
+		}
+
+		document.body.onkeyup = function(e)
+		{
+			// Sets key-up state
+			Modules.Input.Keystates[e.keyCode] = false;
+		}
 	}
 
 	Birde.A = Birde.a = Birde.Actor = function(id, props)
@@ -85,11 +243,15 @@
 			y : 0,
 			w : 0,
 			h : 0,
-			class : "default"
+			class : ["default"]
 		});
 
 		Birde.fn.Scene.Actors[id] = new Actor(id, props);
-		return Birde.fn.Scene.Actors[id];
+
+		var a = new ActorGroup();
+		a.push(Birde.fn.Scene.Actors[id]);
+
+		return a;
 	}
 
 	Birde.fn.Props =
@@ -156,8 +318,6 @@
 			var attr = arguments[0];
 			var value = arguments[1];
 
-			console.log(arguments[1]);
-
 			this.each(function(e)
 			{
 				e[attr] = value;
@@ -167,8 +327,76 @@
 		return this;
 	}
 
+	ActorGroup.prototype.bind = function(event, response)
+	{
+		// Check if we also have a subevent class to deal with
+		if (event.indexOf("[") != -1 && event.indexOf("]") != -1)
+		{
+			// We also have a subevent to use
+			var subEventStart = event.indexOf("[");
+			var subEventEnd = event.indexOf("]");
 
-	// Actor
+			var mainEvent = event.substr(0, subEventStart);
+			var subEvent = event.substr(subEventStart + 1, subEventEnd - subEventStart - 1);
+
+			// standard bind event
+			if (typeof EventRegistry[mainEvent] == 'undefined')
+				EventRegistry[mainEvent] = {};
+
+			// Subevent container
+			if (typeof SubEventRegistry[mainEvent] == 'undefined')
+				SubEventRegistry[mainEvent] = {};
+
+			if (typeof SubEventRegistry[mainEvent][subEvent] == 'undefined')
+				SubEventRegistry[mainEvent][subEvent] = {};
+
+			this.each(function(e)
+			{
+				SubEventRegistry[mainEvent][subEvent][e.id] =
+				{
+					target : e,
+					response : response
+				}
+			});
+
+			console.log(SubEventRegistry);
+		}
+		else
+		{
+			// standard bind event
+			if (typeof EventRegistry[event] == 'undefined')
+				EventRegistry[event] = {};
+
+			this.each(function(e)
+			{
+				EventRegistry[event][e.id] =
+				{
+					target : e,
+					response : response
+				}
+			});
+		}
+		return this;
+	}
+
+	ActorGroup.prototype.move = function(dir)
+	{
+		var x = dir.x * Tick;
+		var y = dir.y * Tick;
+
+		this.each(function(e)
+		{
+			e.x += x;
+			e.y += y;
+		});
+	}
+
+	ActorGroup.prototype.keyMovement = function(speed, keydir)
+	{
+
+	}
+
+	// Actor class - everything on screen is an Actor
 	var Actor = function(id, props)
 	{
 
@@ -176,9 +404,40 @@
 
 		this.id = id;
 
+		this.class = props.class.split(" ");
+
 		this.destroy = function()
 		{
 			Birde.fn.Scene.Actors[this.id] = "";
 		}
+
+		this.move = function(dir)
+		{
+			var x = dir.x * Tick;
+			var y = dir.y * Tick;
+
+			this.x += x;
+			this.y += y;
+		}
+	}
+
+	// Used for maintaining bound/unbound actor events
+	var EventRegistry =
+	{
+		draw : {},
+		step : {},
+		keydown : {},
+		keyup : {},
+		keypress : {}
+	}
+
+	var SubEventRegistry =
+	{
+		draw : {},
+		step : {},
+		keydown : {},
+		keyup : {},
+		keypress : {},
+		keyisdown : {}
 	}
 })(window);
